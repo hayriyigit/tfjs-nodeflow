6226871843a0e97af5a0e88f1b1c1f22b2847922
hayriyigit <hayriyigit@protonmail.com>
2021-01-03 21:16:52 +0300

    Layer elements updated

--- node-elements/index.js
+++ node-elements/index.js
@@ -1,171 +1,60 @@
 import shortid from "shortid";
+import { elements } from "./initial-elements";
 
-export default (node) => {
-  switch (node) {
-    case "INPUT":
-      return {
-        id: shortid.generate(),
-        type: "input",
-        data: {
-          label: "INPUT",
-          args: {
-            row: 1,
-            column: 1,
-            channel: 1,
-            shape: [1, 1, 1],
-          },
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 250, y: 25 },
-        connectable: true,
-        draggable: true,
-        selectable: true,
-      };
+const createElement = (label) => ({
+  id: shortid.generate(),
+  data: {
+    label,
+    args: elements[label].args,
+  },
+  sourcePosition: "right",
+  targetPosition: "left",
+  position: { x: 250, y: 25 },
+  connectable: true,
+  draggable: true,
+  selectable: label == "Concatenate" ? false : true,
+  style: {
+    background: elements[label].color,
+    color: "#000",
+    border: "1px solid #000",
+    width: 100,
+    borderRadius: 0,
+  },
+});
+
+export default (label) => {
+  switch (label) {
+    // case "INPUT":
+    //   return {
+    //     id: shortid.generate(),
+    //     type: "input",
+    //     data: {
+    //       label: "INPUT",
+    //       args: {
+    //         row: 1,
+    //         column: 1,
+    //         channel: 1,
+    //         shape: [1, 1, 1],
+    //       },
+    //     },
+    //     sourcePosition: "right",
+    //     targetPosition: "left",
+    //     position: { x: 250, y: 25 },
+    //     connectable: true,
+    //     draggable: true,
+    //     selectable: true,
+    //   };
     case "Conv2D":
-      return {
-        id: shortid.generate(),
-        data: {
-          label: "Conv2D",
-          args: {
-            filters: 1,
-            kernelSize: 1,
-            strides: 1,
-            padding: "valid",
-            activation: "relu",
-            kernelInitializer: "heUniform",
-          },
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 250, y: 25 },
-        connectable: true,
-        draggable: true,
-        selectable: true,
-        style: {
-          background: "#E06666",
-          color: "#000",
-          border: "1px solid #000",
-          width: 100,
-          borderRadius: 0,
-        },
-      };
+      return createElement(label);
     case "MaxPool2D":
-      return {
-        id: shortid.generate(),
-        data: {
-          label: "MaxPool2D",
-          args: {
-            poolSize: 1,
-            strides: 1,
-            padding: "valid",
-            dataFormat: "channelsFirst",
-          },
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 450, y: 450 },
-        connectable: true,
-        draggable: true,
-        selectable: true,
-        style: {
-          background: "#D9D9D9",
-          color: "#000",
-          border: "1px solid #000",
-          width: 100,
-          borderRadius: 0,
-        },
-      };
+      return createElement(label);
     case "Dense":
-      return {
-        id: shortid.generate(),
-        data: {
-          label: "Dense",
-          args: {
-            units: 1,
-            activation: "relu",
-            kernelInitializer: "heUniform",
-          },
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 450, y: 450 },
-        connectable: true,
-        draggable: true,
-        selectable: true,
-        style: {
-          background: "#6D9EEB",
-          color: "#000",
-          border: "1px solid #000",
-          width: 100,
-          borderRadius: 0,
-        },
-      };
+      return createElement(label);
     case "Dropout":
-      return {
-        id: shortid.generate(),
-        data: {
-          label: "Dropout",
-          args: {
-            rate: 0,
-          },
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 450, y: 450 },
-        connectable: true,
-        draggable: true,
-        selectable: true,
-        style: {
-          background: "#93C47D",
-          color: "#000",
-          border: "1px solid #000",
-          width: 100,
-          borderRadius: 0,
-        },
-      };
+      return createElement(label);
     case "Flatten":
-      return {
-        id: shortid.generate(),
-        data: {
-          label: "Flatten",
-          args: {
-            dataFormat: "channelFirst",
-          },
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 450, y: 450 },
-        connectable: true,
-        draggable: true,
-        selectable: true,
-        style: {
-          background: "#FFD966",
-          color: "#000",
-          border: "1px solid #000",
-          width: 100,
-          borderRadius: 0,
-        },
-      };
+      return createElement(label);
     case "Concatenate":
-      return {
-        id: shortid.generate(),
-        data: {
-          label: "Concatenate",
-        },
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: { x: 450, y: 450 },
-        connectable: true,
-        draggable: true,
-        selectable: false,
-        style: {
-          background: "#B4A7D6",
-          color: "#000",
-          border: "1px solid #000",
-          width: 100,
-          borderRadius: 0,
-        },
-      };
+      return createElement(label);
   }
 };
---
+++ node-elements/initial-elements.js
@@ -0,0 +1,46 @@
+export const elements = {
+  Conv2D: {
+    args: {
+      filters: 1,
+      kernelSize: 1,
+      strides: 1,
+      padding: "valid",
+      activation: "relu",
+      kernelInitializer: "heUniform",
+    },
+    color: "#E06666",
+  },
+  MaxPool2D: {
+    args: {
+      poolSize: 1,
+      strides: 1,
+      padding: "valid",
+      dataFormat: "channelsFirst",
+    },
+    color: "#D9D9D9",
+  },
+  Dense: {
+    args: {
+      units: 1,
+      activation: "relu",
+      kernelInitializer: "heUniform",
+    },
+    color: "#6D9EEB",
+  },
+  Dropout: {
+    args: {
+      rate: 0,
+    },
+    color: "#93C47D",
+  },
+  Flatten: {
+    args: {
+      dataFormat: "channelFirst",
+    },
+    color: "#FFD966",
+  },
+  Concatenate: {
+    args: {},
+    color: "#B4A7D6",
+  },
+};
